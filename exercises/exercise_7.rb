67ce373b2a8fd13026f4daa7dc4978017d8d5788
Rapha321 <ralpha19@hotmail.com>
2019-01-16 01:43:53 +0000

    added validation

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,11 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Give me a store name please"
+print "> "
+store_name = gets.chomp
+
+newStore = Store.create(name: store_name)
+  newStore.errors.messages.each do |error|
+    puts error
+  end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validate :carry_men_or_women
+
+private
+  def carry_men_or_women
+    if !(mens_apparel || womens_apparel)
+      errors.add(:mens_apparel, "Needs to at least serve men's or women's wear")
+      errors.add(:womens_apparel, "Needs to at least serve men's or women's wear")
+    end
+  end
 end
